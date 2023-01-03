dea10d2cae89abb53d801f2ed4638fd492ee9e0e
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2023-01-03 00:03:35 -0300

    Estruturando o projeto - aula 18

--- projeto-curso-react/src/App.jsx
+++ projeto-curso-react/src/App.jsx
@@ -1,9 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Cost</p>
-    </div>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo projeto</Link>
+      </div>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route path='/company' element={<Company/>}/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/newproject' element={<NewProject/>}/>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
-}
+};
 
 export default App;
