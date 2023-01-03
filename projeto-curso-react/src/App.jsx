8eba2c4bf76ab30c086a4b47422c274910543ef5
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2023-01-03 10:26:21 -0300

    Navbar e footer - aula 19

--- projeto-curso-react/src/App.jsx
+++ projeto-curso-react/src/App.jsx
@@ -1,30 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo projeto</Link>
-      </div>
+      <Navbar/>
       <Container customClass='min-height'>
         <Routes>
-          <Route path='/company' element={<Company/>}/>
           <Route path='/' element={<Home/>}/>
+          <Route path='/company' element={<Company/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/newproject' element={<NewProject/>}/>
+          <Route path='/projects' element={<Projects/>}/>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 };
